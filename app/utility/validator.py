efabb3db603a1bb0eb714e8541d18823ed26628a
zanzanzanm03 <olafminister12365@hotmail.com>
2020-09-06 13:43:46 +0700

    add validate name

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,4 +1,18 @@
 def validate_name(name):
+
+    digit = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    special_char = ["!", "@", "#", "$"]
+    space = " "
+
+    if name == "":
+        return False
+
+    for char in name:
+        is_digit = char in digit
+        is_special_char = char in special_char
+        is_space = char == space
+        if is_digit or is_special_char or is_space:
+            return False
     return True
 
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,25 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 class TestUtility(unittest.TestCase):
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("por"))
+
+    def test_validate_name_with_valid_input_string_of_int(self):
+        self.assertEqual(False, validate_name("007"))
+
+    def test_validate_name_with_valid_input_string_of_int(self):
+        self.assertEqual(False, validate_name("ชายปอzaa007"))
+
+    def test_validate_name_with_valid_input_string_of_specail_characters(self):
+        self.assertEqual(False, validate_name("!!"))
+
+    def test_validate_name_with_valid_input_string_of_specail_characters2(self):
+        self.assertEqual(False, validate_name("$อะไร!!เนี่ย$"))
+
+    def test_validate_name_with_valid_empty_input(self):
+        self.assertEqual(False, validate_name(""))
+
+    def test_validate_name_with_valid_spacber_input(self):
+        self.assertEqual(False, validate_name("ปอ ซ่า"))
 
 
 if __name__ == '__main__':
